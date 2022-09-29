08c290f8c3de61332c4abdab5fc384fb82e6cd1d
DillonLin <dillonlintrade@outlook.com>
2022-09-29 14:45:25 +1000

    Avalon branch

--- src/rewards/purgeRewards.ts
+++
@@ -1,9 +0,0 @@
-import { loadArgsAndEnv } from "../utils";
-import initializeDB from "../utils/mongo";
-import { purgeRewardEvents } from "../utils/rewards";
-
-loadArgsAndEnv(process.argv);
-initializeDB()
-  .then(async () => await purgeRewardEvents())
-  .then(() => process.exit(0))
-  .catch(() => process.exit(1));
